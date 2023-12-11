cc40590b0946dec890a7bb280035e90c6b416215
nashgabs <103861343+nashgabs@users.noreply.github.com>
2023-12-11 19:53:33 +0000

    week 11

---
+++ notebooks/main.py
@@ -0,0 +1,7 @@
+#define function to read csv data into an array given a filename
+def opencsv(file):
+    with open("".join( ["data/",file] ), 'r') as f:
+        r = csv.reader(f)
+        data = list(r)
+    #return array of values
+    return np.array(data,dtype=float)
